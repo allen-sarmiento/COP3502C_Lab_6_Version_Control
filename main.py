c17ccceca4a1533fda9464d7b2e6aba79af87b3f
Allen Sarmiento <allenbsarmiento01@gmail.com>
2023-03-07 11:34:48 -0500

    Encode function finished.

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+# TODO: Write main script
 def main():
-    # TODO: Write main script
+    # Showing how encode works
+    raw_test_password = "00009962"
+    encoded_test_password = encode(raw_test_password)
+    print(f"Raw Test Password: {raw_test_password}")
+    print(f"Encoded Test Password: {encoded_test_password}")
 
-def encode():
-    # TODO: Write encode function
 
+def encode(password):
+    '''
+    :param password: Expecting an 8-digit password as a string
+    :return: New encoded 8-digit string with each digit shifted up by 3
+    '''
+    encoded_password = ""
+    for digit in password:
+        # Cast digit to an int and add 3. % 10 to get the first digit after adding.
+        digit = (int(digit)+3) % 10
+        # Add each digit to a new string
+        encoded_password += str(digit)
+    return encoded_password
+
+
+# TODO: Write decode function
 def decode():
-    # TODO: Write decode function
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     main()
