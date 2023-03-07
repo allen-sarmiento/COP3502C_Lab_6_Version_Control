2bbff88062608507fc0f407679ec5c0e65d665a4
Allen Sarmiento <allenbsarmiento01@gmail.com>
2023-03-07 11:05:58 -0500

    Starter template. Please implement the main() and decode() functions. I will implement the encode function.

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    # TODO: Write main script
+
+def encode():
+    # TODO: Write encode function
+
+def decode():
+    # TODO: Write decode function
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
